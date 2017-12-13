ecee55f1c74861878a153544789c373a737008e7
JacksonMcGehee <jacksonmcgehee@gmail.com>
2017-12-12 22:15:06 -0500

    Cleaned up the styling; added more data for the questions and answers.

--- js/app.js
+++ js/app.js
@@ -4,15 +4,157 @@ const gameStuff = [
     // First Category
     {
         givenAnswer: "This is the color of the rain in the famed Prince album.",
-        choices: ['Gurple', 'Whorple', 'Purple', 'Acid'],
-        rightChoice: 'Purple',
+        choices: [' What is gurple', ' What is whorple', ' What is purple', ' What is acid'],
+        rightChoice: ' What is purple',
         tileValue: 100
     },
     {
         givenAnswer: "This is the color of the loveable dinosaur Barney.",
-        choices: ['Shurple', 'Purple', 'Puke', 'Nurple'],
-        rightChoice: 'Purple',
+        choices: [' What is shurple', ' What is purple', ' What is puke', ' What is nurple'],
+        rightChoice: ' What is purple',
         tileValue: 200
+    },
+    {
+        givenAnswer: "This is the color of the top-shelf drink 'Sizzurp'.",
+        choices: [' What is qurple', ' What is purple', ' What is purtple', ' Me an my drank'],
+        rightChoice: ' What is purple',
+        tileValue: 300
+    },
+    {
+        givenAnswer: "It was recently confirmed that this color Teletubby was, in fact, gay.",
+        choices: [' What is purple', ' What is who GAF?', ' What is twurple', ' What is quetzalcoatlurple'],
+        rightChoice: ' What is purple',
+        tileValue: 400
+    },
+    {
+        givenAnswer: "This is the color of the inside of a marmoset's stomach.",
+        choices: [' There is no way you know', ' What is purple', ' What is pneumurple', ' What is vurple'],
+        rightChoice: ' What is purple',
+        tileValue: 500
+    },
+    // Second Category
+    {
+        givenAnswer: "This group is cooler than a polar bear's toenails.",
+        choices: [' Who is Goodie MOB', ' Who is OutKast', ' Who is 2Pac (RIP)', ' Who is Snow'],
+        rightChoice: ' Who is OutKast',
+        tileValue: 100
+    },
+    {
+        givenAnswer: "This group reached number 7 on the charts with 'Coolin' at tha Playground Ya Know!'",
+        choices: [' Who is Kris Kross', ' Who is Bel Biv Devoe', ' Who is Another Bad Creation', ' Who is Hanson'],
+        rightChoice: ' Who is Another Bad Creation',
+        tileValue: 200
+    },
+    {
+        givenAnswer: "Jerry Glanville and the Atlanta Falcons made a cameo appearance in this music video.",
+        choices: [' What is Remember the Time', ' What is Too Legit To Quit', ' What is Slam', ' What is Gin and Juice'],
+        rightChoice: ' What is Too Legit To Quit',
+        tileValue: 300
+    },
+    {
+        givenAnswer: "E.D.I. Mean said this group was 'Softer than Alize with a chaser.'",
+        choices: [' Who is Lil Kim', ' Who is Bad Boy (As a staff, record label & crew)', ' Who is Mobb Deep', ' What is all of them'],
+        rightChoice: ' What is all of them',
+        tileValue: 400
+    },
+    {
+        givenAnswer: "This group had a member named Lil White that I met one time in a Hibbet's Sports in Rome, GA.",
+        choices: [' Who is ThreeSix Mafia', ' Who is 8Ball and MJG', ' Who is No Limit Records', ' Who is Boys II Men'],
+        rightChoice: ' Who is ThreeSix Mafia',
+        tileValue: 500
+    },
+    // Third Category
+    {
+        givenAnswer: "This group is cooler than a polar bear's toenails.",
+        choices: [' Who is Goodie MOB', ' Who is OutKast', ' Who is 2Pac (RIP)', ' Who is Snow'],
+        rightChoice: " What is News Radio",
+        tileValue: 100
+    },
+    {
+        givenAnswer: "This group reached number 7 on the charts with 'Coolin' at tha Playground Ya Know!'",
+        choices: [' Who is Kris Kross', ' Who is Bel Biv Devoe', ' Who is Another Bad Creation', ' Who is Hanson'],
+        rightChoice: " What is Herman's Head",
+        tileValue: 200
+    },
+    {
+        givenAnswer: "Jerry Glanville and the Atlanta Falcons made a cameo appearance in this music video.",
+        choices: [' What is Remember the Time', ' What is Too Legit To Quit', ' What is Slam', ' What is Gin and Juice'],
+        rightChoice: ' What is The Edge',
+        tileValue: 300
+    },
+    {
+        givenAnswer: "E.D.I. Mean said this group was 'Softer than Alize with a chaser.'",
+        choices: [' Who is Lil Kim', ' Who is Bad Boy (As a staff, record label and crew)', ' Who is Mobb Deep', ' What is all of them'],
+        rightChoice: " What is You Can't Do That on Television",
+        tileValue: 400
+    },
+    {
+        givenAnswer: "This group had a member named Lil White that I met one time in a Hibbet's Sports in Rome, GA.",
+        choices: [' Who is ThreeSix Mafia', ' Who is 8Ball and MJG', ' Who is No Limit Records', ' Who is Boys II Men'],
+        rightChoice: " What is Parker Lewis Can't lose",
+        tileValue: 500
+    },
+    // Fourth Category
+    {
+        givenAnswer: "This group is cooler than a polar bear's toenails.",
+        choices: [' Who is Goodie MOB', ' Who is OutKast', ' Who is 2Pac (RIP)', ' Who is Snow'],
+        rightChoice: ' Who is OutKast',
+        tileValue: 100
+    },
+    {
+        givenAnswer: "This group reached number 7 on the charts with 'Coolin' at tha Playground Ya Know!'",
+        choices: [' Who is Kris Kross', ' Who is Bel Biv Devoe', ' Who is Another Bad Creation', ' Who is Hanson'],
+        rightChoice: ' Who is Another Bad Creation',
+        tileValue: 200
+    },
+    {
+        givenAnswer: "Jerry Glanville and the Atlanta Falcons made a cameo appearance in this music video.",
+        choices: [' What is Remember the Time', ' What is Too Legit To Quit', ' What is Slam', ' What is Gin and Juice'],
+        rightChoice: ' What is Too Legit To Quit',
+        tileValue: 300
+    },
+    {
+        givenAnswer: "E.D.I. Mean said this group was 'Softer than Alize with a chaser.'",
+        choices: [' Who is Lil Kim', ' Who is Bad Boy (As a staff, record label and crew)', ' Who is Mobb Deep', ' What is all of them'],
+        rightChoice: ' What is all of them',
+        tileValue: 400
+    },
+    {
+        givenAnswer: "This group had a member named Lil White that I met one time in a Hibbet's Sports in Rome, GA.",
+        choices: [' Who is ThreeSix Mafia', ' Who is 8Ball and MJG', ' Who is No Limit Records', ' Who is Boys II Men'],
+        rightChoice: ' Who is ThreeSix Mafia',
+        tileValue: 500
+    },
+    // Fifth Category
+    {
+        givenAnswer: "This group is cooler than a polar bear's toenails.",
+        choices: [' Who is Goodie MOB', ' Who is OutKast', ' Who is 2Pac (RIP)', ' Who is Snow'],
+        rightChoice: ' Who is OutKast',
+        tileValue: 100
+    },
+    {
+        givenAnswer: "This group reached number 7 on the charts with 'Coolin' at tha Playground Ya Know!'",
+        choices: [' Who is Kris Kross', ' Who is Bel Biv Devoe', ' Who is Another Bad Creation', ' Who is Hanson'],
+        rightChoice: ' Who is Another Bad Creation',
+        tileValue: 200
+    },
+    {
+        givenAnswer: "Jerry Glanville and the Atlanta Falcons made a cameo appearance in this music video.",
+        choices: [' What is Remember the Time', ' What is Too Legit To Quit', ' What is Slam', ' What is Gin and Juice'],
+        rightChoice: ' What is Too Legit To Quit',
+        tileValue: 300
+    },
+    {
+        givenAnswer: "E.D.I. Mean said this group was 'Softer than Alize with a chaser.'",
+        choices: [' Who is Lil Kim', ' Who is Bad Boy (As a staff, record label and crew)', ' Who is Mobb Deep', ' What is all of them'],
+        rightChoice: ' What is all of them',
+        tileValue: 400
+    },
+    {
+        givenAnswer: "This group had a member named Lil White that I met one time in a Hibbet's Sports in Rome, GA.",
+        choices: [' Who is ThreeSix Mafia', ' Who is 8Ball and MJG', ' Who is No Limit Records', ' Who is Boys II Men'],
+        rightChoice: ' Who is ThreeSix Mafia',
+        tileValue: 500
     },
 ]
 
